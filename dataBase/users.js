cffbdaed5bfec492b4dd0db7666166308675c0bb
Huawei <katya.komedeva00@gmail.com>
2021-10-18 20:14:38 +0300

    Initial commit

---
+++ dataBase/users.js
@@ -0,0 +1,7 @@
+module.exports = [
+    {id: 1, name: 'Dima', age: 51},
+    {id: 2, name: 'Katya', age: 41},
+    {id: 3, name: 'Dima', age: 31},
+    {id: 4, name: 'Olya', age: 21},
+    {id: 5, name: 'Max', age: 11}
+]
---
+++ routes/user.router.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+
+const userController = require('../controllers/user.controller');
+
+router.get('/', userController.getUsers);
+
+router.get('/:user_id', userController.getUserById);
+
+router.post('/', userController.createUsers);
+
+router.put('/', userController.updateUsers);
+
+module.exports = router;

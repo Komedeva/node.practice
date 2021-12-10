2e2d9094c18e4167114d19584c60d885965f0095
Huawei <katya.komedeva00@gmail.com>
2021-12-10 17:23:00 +0200

    Node.js

--- app.js
+++ app.js
@@ -2,12 +2,23 @@ const express = require('express');
 
 const app = express();
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({extended: true}));
 
-const userRouter = require ('./routes/user.router');
+// const userRouter = require('./routes/user.router');
 
-app.use('/users', userRouter);
+// app.use('/users', userRouter);
 
-app.listen(5000, ()=>{
+app.listen(5000, () => {
     console.log(`App listen 5000`)
 });
+
+const boys = require('./dataBaseBoys/package.json');
+const girls = require('./dataBaseBoys/package.json');
+
+
+boys.forEach(data=>{
+    if {gender === male
+    }else {
+
+    }
+})
--- dataBase/users.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-    {id: 1, name: 'Dima', age: 51},
-    {id: 2, name: 'Katya', age: 41},
-    {id: 3, name: 'Dima', age: 31},
-    {id: 4, name: 'Olya', age: 21},
-    {id: 5, name: 'Max', age: 11}
-]
